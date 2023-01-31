0ac32e9cd53a4eaaa6520ab33d2474f7da76b3b2
Vadim Chumachenko <vadimchumachenko123@gmail.com>
2023-01-31 21:00:24 +0300

    email validation, get_item

--- ServerUtils/app/search_utils.py
+++ ServerUtils/app/search_utils.py
@@ -1,3 +1,3 @@
 brands = ["nike", "jordan", "puma", "gucci", "adidas", "off white", "reebok"]
-colors = ["black", "white", "red", "yellow", "green", "gray", "blue"]
+colors = ["black", "white", "red", "yellow", "green", "gray", "blue", "beige"]
 types = ["sneakers", "hoodie"]
