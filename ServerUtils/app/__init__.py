43909cf7d4a6a4635b0640c81de82e0b4f240ee3
Vadim Chumachenko <vadimchumachenko123@gmail.com>
2023-02-02 23:53:32 +0300

    added access token

--- ServerUtils/app/__init__.py
+++ ServerUtils/app/__init__.py
@@ -4,5 +4,6 @@ import os
 app = Flask(__name__)
 # connecting app with db
 app.config.update(dict(DATABASE = os.path.join(app.root_path, 'market.db')))
+app.config['SECRET_KEY'] = "skfF142DFG3412alfjdjf421ADdiewfjvVDFGv42342nasfjsSFG231hjdf"
 
 from app import views
---
+++ ServerUtils/app/token.py
@@ -0,0 +1,15 @@
+from app import app
+import jwt
+from datetime import datetime, timedelta
+
+def generate_token(user_id):
+    token = jwt.encode({"user_id" : user_id, 
+    "exp": datetime.utcnow() + timedelta(minutes=30)}, 
+    app.config['SECRET_KEY'], algorithm = "HS256")
+    return token
+
+def decode_token(token):
+    try:
+        return jwt.decode(token, app.config['SECRET_KEY'], algorithms = "HS256")
+    except:
+        return False
